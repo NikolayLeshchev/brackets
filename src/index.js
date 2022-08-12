b67f00e9230569ead5733744ff7ab9bd853f7d92
Nikolay <82165109+NikolayLeshchev@users.noreply.github.com>
2022-08-12 16:38:47 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let array = [];
+  
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      array.push(bracketsConfig[i].join(''))
+  };
+  
+  for (let i = 0; i < array.length; i++) {
+      if (str.includes(array[i])) {
+          str = str.replace(array[i], '');
+          i = -1;
+      }
+   };
+  
+  
+   if (str) {
+       return false
+   } else {
+       return true
+   }
 }
